d585d1313516435c65ce6f009dd5c23120d92f17
hacikhacatran7@gmail.com <hacikhacatran7@gmail.com>
2024-11-20 20:35:36 +0400

    update bttn

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,49 +1,44 @@
 import { InputButtonType } from "@/types/common";
 import { nanoid } from "nanoid";
+
 export const plusButtons: InputButtonType[] = [
-    
     {
         id: nanoid(),
         value: 1,
-        input: 2,
     },
     {
         id: nanoid(),
         value: 2,
-        input: 2,
-    },
-    {
-        id: nanoid(),
-        value: 1,
-        input: 3,
     },
     {
         id: nanoid(),
         value: 3,
-        input: 3,
     },
+    {
+        id: nanoid(),
+        value: 1,
+        input: 2,
+    },
+
 ];
 
 export const minusButtons: InputButtonType[] = [
-   
     {
         id: nanoid(),
         value: 1,
-        input: 2,
     },
     {
         id: nanoid(),
         value: 2,
-        input: 2,
-    },
-    {
-        id: nanoid(),
-        value: 1,
-        input: 3,
     },
     {
         id: nanoid(),
         value: 3,
-        input: 3, 
     },
+    {
+        id: nanoid(),
+        value: 1,
+        input: 2,
+    },
+
 ];
